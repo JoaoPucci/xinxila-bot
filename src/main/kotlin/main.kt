4aad49338e375bb714c327fcf1a31b19d63c5574
Joao Pucci <joao.pucci@live.com>
2023-05-23 00:05:19 +0900

    Add a sample call with kord

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,22 @@
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
+
+private const val XINXILA_DISCORD_BOT_TOKEN = "XINXILA_DISCORD_BOT_TOKEN"
+suspend fun main() {
+
+    val kord = Kord(System.getenv(XINXILA_DISCORD_BOT_TOKEN))
+
+    // Simplified style
+    kord.on<MessageCreateEvent> {
+        if (message.author?.isBot == true) return@on
+        if (message.content == "!ping") message.channel.createMessage("pong")
+    }
+
+    kord.login {
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
+}
